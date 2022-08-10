9c01484df07582ded9d4ff4ad84fa758f515eb18
firozahammed <110807623+firozahammed@users.noreply.github.com>
2022-08-10 15:08:09 +0300

    Update adminPage.py

--- adminPage.py
+++ adminPage.py
@@ -13,40 +13,6 @@ import streamlit as st
 from google.oauth2 import service_account
 from gsheetsdb import connect
 
-def fetch_data(date_from,date_to,rows):
-    with open('report ' + str(date_from) + ' ' + str(date_to) + '.csv', 'a') as f:
-        # using csv.writer method from CSV package
-        dw = csv.DictWriter(f, delimiter=',',
-                            fieldnames=headers)
-        dw.writeheader()
-        for row in rows:
-            write = csv.writer(f)
-            write.writerow(row)
-
-
-
-
-
-# Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=["https://www.googleapis.com/auth/spreadsheets",],)
-conn = connect(credentials=credentials)
-
-def type_to_csv(array):
-    with open('permissions.csv', 'a') as f:
-        # using csv.writer method from CSV package
-        write = csv.writer(f)
-        write.writerow(array)
-
-
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
-
-
 def set_bg_hack(main_bg):
     '''
     A function to unpack an image from root folder and set as bg.
@@ -88,8 +54,32 @@ if radio_selection == 'Print Reports':
         clm1, clm2 = st.columns(2)
         date_from = clm1.date_input('From')
         date_to = clm2.date_input('To')
-        sheet_url = st.secrets["private_gsheets_url"]
+    
+        
+        # Create a connection object.
+        credentials = service_account.Credentials.from_service_account_info(
+            st.secrets["gcp_service_account"],
+            scopes=[
+                "https://www.googleapis.com/auth/spreadsheets",
+            ],
+        )
+        conn = connect(credentials=credentials)
 
+        # Perform SQL query on the Google Sheet.
+        # Uses st.cache to only rerun when the query changes or after 10 min.
+        @st.cache(ttl=600)
+        def run_query(query):
+            rows = conn.execute(query, headers=1)
+            rows = rows.fetchall()
+            return rows
+
+
+        sheet_url = st.secrets["private_gsheets_url"]
+        rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+        # Print results.
+        for row in rows:
+            st.write(f"{row.name} has a :{row.ID}:")
 
         output = BytesIO()
 
