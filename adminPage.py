63a1be88b54b2c0884e5e8dab30010cc6bb593fe
firozahammed <110807623+firozahammed@users.noreply.github.com>
2022-08-11 09:58:51 +0300

    Update adminPage.py

--- adminPage.py
+++ adminPage.py
@@ -75,7 +75,7 @@ if radio_selection == 'Print Reports':
 
 
         sheet_url = st.secrets["private_gsheets_url"]
-        rows = run_query(f'SELECT * FROM "{sheet_url}" WHERE name="ALI"')
+        rows = run_query(f'SELECT * FROM "{sheet_url}" WHERE name=ALI')
 
         # Print results.
         for row in rows:
