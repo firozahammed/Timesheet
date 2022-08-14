9a122c89362d979043a42731f8a9d866f6583465
firozahammed <110807623+firozahammed@users.noreply.github.com>
2022-08-14 17:47:47 +0300

    Update adminPage.py

--- adminPage.py
+++ adminPage.py
@@ -45,13 +45,9 @@ def set_bg_hack(main_bg):
 image = Image.open("OIP.jpg")
 st.image(image)
 
-st.markdown("Main Page")
-st
-
-
-#radio_selection = st.sidebar.selectbox('Choose an option:', ('---','Print Reports', 'Give Exception'))
-#if radio_selection == 'Print Reports':
-#if selected == 'Print Reports':
+radio_selection = st.sidebar.selectbox('Choose an option:', ('---','Print Reports', 'Give Exception'))
+if radio_selection == 'Print Reports':
+if selected == 'Print Reports':
     st.title('Print Reports ')
     select_box_choice = st.selectbox('Print for:', ('--','All', 'Specific Employee'))
     headers = ('ID', 'name', 'date', 'customer1_visit',' customer1_name', 'customer1_country', 'customer1_location',
