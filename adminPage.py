32e315eacc58d03ab752592cbd33913f8e4d4c76
firozahammed <110807623+firozahammed@users.noreply.github.com>
2022-08-17 17:10:42 +0300

    Update adminPage.py

--- adminPage.py
+++ adminPage.py
@@ -10,6 +10,7 @@ import streamlit as st
 import base64
 import datetime as dt
 import streamlit as st
+import numpy as np
 from google.oauth2 import service_account
 from gsheetsdb import connect
 import gspread as gs
@@ -105,8 +106,8 @@ if radio_selection == 'Print Reports':
         #df['Total Time']=pd.to_datetime(df['Total Time'],format='%H:%M:%S',errors='ignore').dt.time
         #df['Total Time'] = pd.to_datetime(df['Total Time'].astype(str)).dt.strftime('%H:%M:%S')
         #df['Total Time'] = df['Total Time'].dt.strptime('%H:%M:%S')
-        df['Total Time'] = pd.to_datetime(df['Total Time'])
-        df=df.groupby(['Employee ID'])
+        df['Total Time'] = np.ptp(pd.to_datetime(df['Total Time']))
+        df=df.groupby(['Employee ID'])['Total Time'].sum()
         #df.dtypes['Total Time']
         df
         #df = df.loc[(df['User ID'].astype(str) == ID) ]
