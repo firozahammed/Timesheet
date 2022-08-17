43f0604934e452d660b017fcce26ebcf2b15b08a
firozahammed <110807623+firozahammed@users.noreply.github.com>
2022-08-17 11:54:27 +0300

    Update adminPage.py

--- adminPage.py
+++ adminPage.py
@@ -48,13 +48,13 @@ st.image(image)
 radio_selection = st.sidebar.radio('Choose an option:', ('Print Reports', 'Give Excuse'))
 if radio_selection == 'Print Reports':
     st.title('Print Reports ')
-    select_box_choice = st.radio('Print for:', ('All', 'Specific Employee'))
+    select_box_choice = st.radio('Print:', ('Daily', 'Summary'))
     headers = ('ID', 'name', 'date', 'customer1_visit',' customer1_name', 'customer1_country', 'customer1_location',
                'customer2_visit','customer2_name','customer2_country','customer2_location','customer3_visit',
                'customer3_name','customer3_country','customer3_location','hospital_visit','hospital_location',
                'vendor1_visit','vendor1_name','vendor2_visit','vendor2_name','business_trip_country','trip_location',
                'date_of_trip','date_of_return','personal_excuse','reporting_late')
-    if select_box_choice == 'All':
+    if select_box_choice == 'Daily':
         clm1, clm2 = st.columns(2)
 
         date_from = clm1.date_input('From').strftime("%Y-%m-%d")
@@ -72,11 +72,19 @@ if radio_selection == 'Print Reports':
         towrite = io.BytesIO()
         with pd.ExcelWriter(towrite,engine='xlsxwriter') as writer:
             df.to_excel(writer,sheet_name='Sheet1',index=False)
+
+            # Autofit excel column header
+            for column in df:
+                column_length = max(df[column].astype(str).map(len).max(), len(column))
+                col_idx = df.columns.get_loc(column)
+                writer.sheets['Sheet1'].set_column(col_idx, col_idx, column_length)
+
+
             writer.save()
 
         download_button=st.download_button(label="Download Report",data=towrite,file_name="Report_"+date_from+".xlsx",mime="application/vnd.ms-excel")
 
-    if select_box_choice == 'Specific Employee':
+    if select_box_choice == 'Summary':
         clm1, clm2, clm3 = st.columns(3)
         ID = clm1.text_input('Enter employee ID:')
         #name = clm2.text_input(label="", value="", disabled=True)
@@ -86,21 +94,23 @@ if radio_selection == 'Print Reports':
         scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
         creds = ServiceAccountCredentials.from_json_keyfile_name("secret.json", scopes=scopes)
         file = gspread.authorize(creds)
-        workbook = file.open("Summary Timesheet")
+        workbook = file.open("Attendance")
         sheet = workbook.sheet1
         sheet_url = st.secrets["private_gsheets_url"]
         df = pd.DataFrame(sheet.get_all_records())
         #df = df.loc[(df['date'] >= date_from) & (df['date'] <= date_to) & (df['ID'].astype(str) == ID) ]
+        df
         df = df.loc[(df['User ID'].astype(str) == ID) ]
         towrite = io.BytesIO()
         with pd.ExcelWriter(towrite, engine='xlsxwriter') as writer:
             df.to_excel(writer, sheet_name='Sheet1',index=False)
 
+            #Autofit excel column header
             for column in df:
                 column_length = max(df[column].astype(str).map(len).max(), len(column))
                 col_idx = df.columns.get_loc(column)
                 writer.sheets['Sheet1'].set_column(col_idx, col_idx, column_length)
-            
+
             writer.save()
 
         download_button = st.download_button(label="Download Report", data=towrite,
