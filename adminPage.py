263e7e7f6c0888d7abf10106893cc83156f8451c
firozahammed <110807623+firozahammed@users.noreply.github.com>
2022-08-17 12:24:09 +0300

    Update adminPage.py

--- adminPage.py
+++ adminPage.py
@@ -102,7 +102,7 @@ if radio_selection == 'Print Reports':
         #df['Date']=pd.to_datetime(df['Date'])
         #df.groupby([pd.Grouper(key='Date')])['Total Time'].sum()
         df['Total Time']=pd.to_timedelta(df['Total Time'])
-        df=df.groupby(['Employee ID'])['Total Time'].sum()
+        #df=df.groupby(['Employee ID'])['Total Time'].sum()
         df
         #df = df.loc[(df['User ID'].astype(str) == ID) ]
         towrite = io.BytesIO()
