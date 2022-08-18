84bf6481fb961897424759bb887eb86acb6b8cf5
firozahammed <110807623+firozahammed@users.noreply.github.com>
2022-08-18 12:21:28 +0300

    Update adminPage.py

--- adminPage.py
+++ adminPage.py
@@ -116,7 +116,7 @@ if radio_selection == 'Print Reports':
 
         df['Total Time'] = (pd.to_timedelta(df['Total Time']).astype('timedelta64[s]').astype(int))/3600
         df = df.loc[(df['Date'] >= "8/1/2022" ) & (df['Date'] <= "8/2/2022")]
-        
+        df = df.groupby([df['Date'].dt.date]).mean()
         df
         #df=df.groupby(['Employee ID'])['Total Time'].sum()
 
