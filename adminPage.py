b84091c8c9838a16911e99c81c7e76e1bbf46855
firozahammed <110807623+firozahammed@users.noreply.github.com>
2022-08-18 15:06:13 +0300

    Update adminPage.py

--- adminPage.py
+++ adminPage.py
@@ -120,7 +120,7 @@ if radio_selection == 'Print Reports':
 
         #df['Date'] = df['Date'].strftime("%m/%d/%Y")
         df = df.loc[(df['Date'] >= "8/1/2022") & (df['Date'] <= "8/2/2022")]
-        df = df.groupby(['Employee ID',df['Date'].strptime('%m/%d/%Y')],as_index=False)['Total Time'].sum()
+        df = df.groupby(['Employee ID',pd.Grouper(df['Date'],freq='D')],as_index=False)['Total Time'].sum()
         df
 
 
