f668bf0c6315cca83fe0745c0001116741c7b400
firozahammed <110807623+firozahammed@users.noreply.github.com>
2022-08-18 09:05:38 +0300

    Update adminPage.py

--- adminPage.py
+++ adminPage.py
@@ -107,7 +107,7 @@ if radio_selection == 'Print Reports':
         #df['Total Time']=pd.to_datetime(df['Total Time'],format='%H:%M:%S',errors='ignore').dt.time
         #df['Total Time'] = pd.to_datetime(df['Total Time'].astype(str)).dt.strftime('%H:%M:%S')
         #df['Total Time'] = df['Total Time'].dt.strptime('%H:%M:%S')
-        df['Total Time']=pd.to_datetime(df['Total Time'],format='%H:%M:%S',errors='ignore').dt.time
+        df['Total Time']=pd.to_datetime(df['Total Time'])
         #df['Total Time'] = df['Total Time'].dt.total_seconds()
 
 
