0a26fa370377bbc8979bf5e97390d3497b74bba3
firozahammed <110807623+firozahammed@users.noreply.github.com>
2022-08-22 09:05:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -49,6 +49,34 @@ image = Image.open("OIP.jpg")
 st.image(image)
 
 
+
+placeholder = st.empty()
+with placeholder.container():
+
+    st.title('Please enter the security key')
+    security_key = st.text_input('Security key')
+
+    df = pd.DataFrame(sheet.get_all_records())
+    check_security_key = (security_key in df['Token'].astype(str).unique())
+    submit_button = st.button("Submit")
+
+    if submit_button:
+
+        if security_key!="":
+                if check_security_key is False:
+                    st.error("The security key: " + security_key + " is invalid.")
+
+
+                else:
+                    placeholder.empty()
+
+
+
+
+        else:
+            st.error("Please enter the security key")
+
+
 FormContainer = st.empty()
 with FormContainer.container():
                         df = pd.DataFrame(sheet.get_all_records())
@@ -59,21 +87,3 @@ with FormContainer.container():
                         # FormContainer.text_input('Employee ID', value=EmployeeID, disabled=True)
 
                         st.title("Dear , you have been late for today\'s attendance")
-
-
-
-def CheckSecurityKey(security_key):
-    df = pd.DataFrame(sheet.get_all_records())
-    check_security_key = (security_key in df['Token'].astype(str).unique())
-    if check_security_key is False:
-        st.error("The security key: " + security_key + " is invalid.")
-    else:
-        placeholder.empty()
-
-placeholder = st.empty()
-with placeholder.container():
-
-    st.title('Please enter the security key')
-    security_key = st.text_input('Security key')
-    submit_button = st.button("Submit", on_click=CheckSecurityKey(security_key))
-
