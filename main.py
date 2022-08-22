3a3fe06e86f528f23244141f1b22553e8ddf6d85
firozahammed <110807623+firozahammed@users.noreply.github.com>
2022-08-22 13:33:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PIL import Image
 
 # streamlit_app.py
 from datetime import date
+from datetime import datetime
 import streamlit as st
 import pandas as pd
 import gspread
@@ -66,15 +67,88 @@ with TokenContainer.container():
                             df = df.loc[(df['Token'].astype(str) == str(security_key))]
                             EmployeeName = df['Name'].values[0]
                             EmployeeID = df['User ID'].values[0]
-                            ReportingTime = df['Reporting Time'].values[0]
+                            EmployeeToken = df['User ID'].values[0]
+                            ReportingTime = df['Token'].values[0]
+                            ReportingDate = date.today().strftime("%m/%d/%y")
                             st.title("Dear " + EmployeeName + ", you have been late for today "+date.today().strftime("%m/%d/%y"))
                             st.text_input('Employee ID', value=EmployeeID, disabled=True)
                             st.text_input('Reporting Time',value=ReportingTime,disabled=True)
-                            options = ('Customer visit','Hospital visit','Vendor visit','Business trip','Personal excuse','Reporting late')
-                            selection = st.selectbox("Please choose a reason",options)
+                            options = ('Customer visit','Hospital visit','Vendor visit','Business trip','Personal','Reporting late')
+                            reason = st.selectbox("Please choose a reason",options)
+
+                            scopes = ['https://www.googleapis.com/auth/spreadsheets',
+                                      'https://www.googleapis.com/auth/drive']
+                            creds = ServiceAccountCredentials.from_json_keyfile_name("secret.json", scopes=scopes)
+                            file = gspread.authorize(creds)
+                            workbook = file.open("Timesheet")
+                            sheet = workbook.sheet1
+                            sheet_url = st.secrets["private_gsheets_url"]
+
+                            exemption_list = []
+                            details = []
+
+                            if reason == 'Customer site':
+                                clm1, clm2, clm3, clm4, clm5 = st.columns(5)
+                                client_name = clm1.text_input('Client name:')
+                                client_loc = clm3.text_input('Location:', key=1)
+                                country = clm2.text_input('Country:', key=3)
+                                from_time = clm4.time_input('From:', datetime.now(), 1)
+                                to_time = clm5.time_input('To:', datetime.now(), 1)
+                                details = ['Client:' + client_name, 'Location:' + client_loc, 'Country:' + country]
+                                details = '\n\n'.join(details)
+                                save_add_button = clm1.button('Add')
+                                if save_add_button:
+                                    details_list = [EmployeeToken,EmployeeID,EmployeeName,ReportingDate,str(from_time), str(to_time), reason, details]
+                                    sheet.append_row(details_list)
+                                    st.success('Successfully added!')
+                                    st.stop()
+                            elif reason == 'Medical':
+                                clm1, clm2, clm3 = st.columns(3)
+                                hospital_name = clm1.text_input('Hospital name:')
+                                from_time = clm2.time_input('From:', datetime.now(), 1)
+                                to_time = clm3.time_input('To:', datetime.now(), 1)
+                                save_add_button = clm1.button('Add')
+                                if save_add_button:
+                                    details_list = [EmployeeToken,EmployeeID,EmployeeName,ReportingDate,str(from_time), str(to_time), reason,'Hospital:' + hospital_name]
+                                    sheet.append_row(details_list)
+                                    st.success('Successfully added!')
+                                    st.stop()
+
+                            elif reason == 'Vacation':
+                                clm1, clm2 = st.columns(2)
+                                from_time = clm1.time_input('From:', datetime.now(), 1)
+                                to_time = clm2.time_input('To:', datetime.now(), 1)
+                                save_add_button = clm1.button('Add')
+                                if save_add_button:
+                                    details_list = [EmployeeToken,EmployeeID,EmployeeName,ReportingDate,str(from_time), str(to_time), reason]
+                                    sheet.append_row(details_list)
+                                    st.success('Successfully added!')
+                                    st.stop()
+
+
+                            elif reason == 'Personal':
+
+                                personal_details = st.text_area('Enter details', height=None)
+                                clm1, clm2 = st.columns(2)
+                                from_time = clm1.time_input('From:', datetime.now(), 1)
+                                to_time = clm2.time_input('To:', datetime.now(), 1)
+                                details = []
+                                details = personal_details
+                                save_add_button = clm1.button('Add')
+                                if save_add_button:
+                                    details_list = [EmployeeToken,EmployeeID,EmployeeName,ReportingDate,str(from_time), str(to_time), reason, details]
+                                    sheet.append_row(details_list)
+                                    st.success('Successfully added!')
+                                    st.stop()
+
+
+
+
+
+
 
             else:
-                    st.warning('Security key is mandatory')
+                    st.warning('Note: Security key is mandatory')
 
 
 
