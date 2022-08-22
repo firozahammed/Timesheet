3c7cde5a0ad51a3c2d87e558020334d3e9e63171
firozahammed <110807623+firozahammed@users.noreply.github.com>
2022-08-22 09:33:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -49,10 +49,19 @@ image = Image.open("OIP.jpg")
 st.image(image)
 
 
-import time
-with st.empty():
-    for seconds in range(60):
-        st.write(f"⏳ {seconds} seconds have passed")
-        time.sleep(1)
-    st.write("✔️ 1 minute over!")
+placeholder = st.empty()
+
+# Replace the placeholder with some text:
+placeholder.text("Hello")
+
+# Replace the text with a chart:
+placeholder.line_chart({"data": [1, 5, 2, 6]})
+
+# Replace the chart with several elements:
+with placeholder.container():
+     st.write("This is one element")
+     st.write("This is another")
+
+# Clear all those elements:
+placeholder.empty()
 
