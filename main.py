1d3893939d84a445710dfd739acbf3385dbe4710
firozahammed <110807623+firozahammed@users.noreply.github.com>
2022-08-23 15:46:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ if st.session_state['step'] == 0:
 
 if st.session_state['step'] == 1:
 
-        st.write("Security key -"+str(security_key))
+        
         with st.form(key='EmployeeForm'):
             scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
             creds = ServiceAccountCredentials.from_json_keyfile_name("secret.json", scopes=scopes)
