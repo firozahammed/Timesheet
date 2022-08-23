36cb71c0d3ef71d024aad98ca6da37ebe93c48c9
firozahammed <110807623+firozahammed@users.noreply.github.com>
2022-08-23 17:04:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import base64
 
-
 from PIL import Image
 
 # streamlit_app.py
@@ -9,7 +8,6 @@ import streamlit as st
 import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-
 
 
 # setting the form background
@@ -36,11 +34,22 @@ def set_bg_hack(main_bg):
         unsafe_allow_html=True
     )
 
-#set_bg_hack('background.png')
+
+# set_bg_hack('background.png')
 
 
 image = Image.open("OIP.jpg")
 st.image(image)
+
+def DropDown():
+    if st.session_state['reason'] =='Medical':
+        clm1, clm2, clm3 = st.columns(3)
+        hospital_name = clm1.text_input('Hospital name:')
+        from_time = clm4.time_input('From:')
+        to_time = clm5.time_input('To:')
+        
+
+
 
 
 
@@ -52,7 +61,7 @@ if st.session_state.get('security_key') is None:
 
 if st.session_state['step'] == 0:
 
-    with st.form(key = 'TokenForm'):
+    with st.form(key='TokenForm'):
         scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
         creds = ServiceAccountCredentials.from_json_keyfile_name("secret.json", scopes=scopes)
         file = gspread.authorize(creds)
@@ -74,59 +83,114 @@ if st.session_state['step'] == 0:
                 st.error("The security key: " + security_key + " is invalid.")
             else:
                 st.session_state['step'] = 1
-                #st.experimental_rerun()
+                # st.experimental_rerun()
 
 
     else:
         st.info('Note: Security key is mandatory')
 
-
-
 if st.session_state['step'] == 1:
 
+    with st.form(key='EmployeeForm'):
 
-        with st.form(key='EmployeeForm'):
+        scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+        creds = ServiceAccountCredentials.from_json_keyfile_name("secret.json", scopes=scopes)
+        file = gspread.authorize(creds)
+        workbook = file.open("Summary Timesheet")
+        sheet = workbook.sheet1
+        sheet_url = st.secrets["private_gsheets_url"]
+        df = pd.DataFrame(sheet.get_all_records())
+        security_key = st.session_state.get('security_key')
+        df = df.loc[(df['Token'].astype(str) == str(security_key))]
+        EmployeeName = df['Name'].values[0]
+        EmployeeID = df['User ID'].values[0]
+        EmployeeToken = df['Token'].values[0]
+        ReportingTime = df['Reporting Time'].values[0]
+        ReportingDate = date.today().strftime("%m/%d/%y")
+        st.title("Dear " + EmployeeName + ", you have been late for today " + date.today().strftime(
+            "%m/%d/%y"))
+        st.text_input('Employee ID', value=EmployeeID, disabled=True)
+        st.text_input('Reporting Time', value=ReportingTime, disabled=True)
+        options = (
+            'Customer site', 'Medical', 'Business trip', 'Personal', 'Reporting late')
+        reason = st.selectbox("Please choose a reason", options, on_change=DropDown, key="reason")
+        st.session_state['reason']=reason
+        
+        
+        scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+        creds = ServiceAccountCredentials.from_json_keyfile_name("secret.json", scopes=scopes)
+        file = gspread.authorize(creds)
+        workbook = file.open("Timesheet")
+        sheet = workbook.sheet1
+        sheet_url = st.secrets["private_gsheets_url"]
 
-            scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-            creds = ServiceAccountCredentials.from_json_keyfile_name("secret.json", scopes=scopes)
-            file = gspread.authorize(creds)
-            workbook = file.open("Summary Timesheet")
-            sheet = workbook.sheet1
-            sheet_url = st.secrets["private_gsheets_url"]
-            df = pd.DataFrame(sheet.get_all_records())
-            security_key = st.session_state.get('security_key')
-            df = df.loc[(df['Token'].astype(str) == str(security_key))]
-            EmployeeName = df['Name'].values[0]
-            EmployeeID = df['User ID'].values[0]
-            EmployeeToken = df['Token'].values[0]
-            ReportingTime = df['Reporting Time'].values[0]
-            ReportingDate = date.today().strftime("%m/%d/%y")
-            st.title("Dear " + EmployeeName + ", you have been late for today " + date.today().strftime(
-                "%m/%d/%y"))
-            st.text_input('Employee ID', value=EmployeeID, disabled=True)
-            st.text_input('Reporting Time', value=ReportingTime, disabled=True)
-            options = (
-                'Customer site', 'Medical', 'Business trip', 'Personal', 'Reporting late')
-            reason = st.selectbox("Please choose a reason", options, key="reason")
+        details_list = []
+        details = []
 
-            scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-            creds = ServiceAccountCredentials.from_json_keyfile_name("secret.json", scopes=scopes)
-            file = gspread.authorize(creds)
-            workbook = file.open("Timesheet")
-            sheet = workbook.sheet1
-            sheet_url = st.secrets["private_gsheets_url"]
+        if reason == 'Customer site':
+            clm1, clm2, clm3, clm4, clm5 = st.columns(5)
+            client_name = clm1.text_input('Client name:')
+            country = clm2.text_input('Country:', key=3)
+            client_loc = clm3.text_input('Location:', key=1)
+            from_time = clm4.time_input('From:')
+            to_time = clm5.time_input('To:')
+            details = ['Client:' + client_name, 'Location:' + client_loc, 'Country:' + country]
+            details = '\n\n'.join(details)
+            add_button = st.form_submit_button(label="Add")
+            if add_button:
+                details_list = [str(EmployeeToken), str(EmployeeID), str(EmployeeName),
+                                date.today().strftime("%m/%d/%Y"),
+                                str(from_time), str(to_time),
+                                reason, details]
+                sheet.append_row(details_list)
+                st.success('Successfully added!')
 
-            details_list = []
-            details = []
 
-            if reason:
-                if st.session_state["reason"]=='Customer site':
-                    st.session_state["reason"]=st.columns(5)
-                    clm1, clm2, clm3, clm4, clm5 = st.columns(5)
-            
-                if st.session_state["reason"] == 'Medical':
-                    st.session_state["reason"] = st.columns(3)
-                    clm1, clm2, clm3 = st.columns(3)
-                    
+            elif reason == 'Medical':
+                clm1, clm2, clm3 = st.columns(3)
+                hospital_name = clm1.text_input('Hospital name:')
+                from_time = clm4.time_input('From:')
+                to_time = clm5.time_input('To:')
+                save_add_button = clm1.button('Add')
+                if save_add_button:
+                    details_list = [EmployeeToken, EmployeeID, EmployeeName, date.today().strftime("%m/%d/%Y"),
+                                    str(from_time),
+                                    str(to_time), reason, 'Hospital:' + hospital_name]
+                    sheet.append_row(details_list)
+                    st.success('Successfully added!')
+
+
+
+            elif reason == 'Business trip':
+                clm1, clm2 = st.columns(2)
+                from_time = clm1.time_input('From:')
+                to_time = clm2.time_input('To:')
+                save_add_button = clm1.button('Add')
+                if save_add_button:
+                    details_list = [EmployeeToken, EmployeeID, EmployeeName, date.today().strftime("%m/%d/%Y"),
+                                    str(from_time),
+                                    str(to_time), reason]
+                    sheet.append_row(details_list)
+                    st.success('Successfully added!')
+
+
+
+
+            elif reason == 'Personal':
+
+                personal_details = st.text_area('Enter details', height=None)
+                clm1, clm2 = st.columns(2)
+                from_time = clm1.time_input('From:')
+                to_time = clm2.time_input('To:')
+                details = []
+                details = personal_details
+                save_add_button = clm1.button('Add')
+                if save_add_button:
+                    details_list = [EmployeeToken, EmployeeID, EmployeeName, date.today().strftime("%m/%d/%Y"),
+                                    str(from_time),
+                                    str(to_time), reason, details]
+                    sheet.append_row(details_list)
+                    st.success('Successfully added!')
+
 
 
