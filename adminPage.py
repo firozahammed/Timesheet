935a67d4f560e9022c513c21266c67163f00bb47
firozahammed <110807623+firozahammed@users.noreply.github.com>
2022-08-24 12:30:13 +0300

    Update adminPage.py

--- adminPage.py
+++ adminPage.py
@@ -142,7 +142,7 @@ if radio_selection == 'Print Reports':
         towrite = io.BytesIO()
         with pd.ExcelWriter(towrite, engine='xlsxwriter') as writer:
 
-            worksheet.cell(row=0, column=0).value = 'Date:'+str(date_from)+" - "+str(date_to)
+            sheet.cell(row=0, column=0).value = 'Date:'+str(date_from)+" - "+str(date_to)
             df.to_excel(writer, sheet_name='Sheet1',index=False, startcol=0,startrow=2)
 
             #Autofit excel column header
